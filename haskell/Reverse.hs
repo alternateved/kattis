f54c1d2add4ed71be2672ca0ff5abb4e09236f09
alternateved <alternateved@gmail.com>
2021-11-30 22:45:09 +0100

    Rework Reverse

--- haskell/Reverse.hs
+++ haskell/Reverse.hs
@@ -1,19 +1,7 @@
 module Main where
 
-import Control.Monad (replicateM)
-
-getInt :: IO Int
-getInt = readLn
-
-getLines :: Int -> IO [Int]
-getLines n = replicateM n getInt
-
-putReversed :: [Int] -> IO ()
-putReversed [] = return ()
-putReversed (x : xs) = putReversed xs >> print x
+run :: String -> String
+run = unlines . reverse . tail . lines
 
 main :: IO ()
-main = do
-  n <- getInt
-  ls <- getLines n
-  putReversed ls
+main = interact run
