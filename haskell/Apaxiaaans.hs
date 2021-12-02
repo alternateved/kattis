30ac89d8bdf0199d7af39b35f9bec50b6e3830ce
alternateved <alternateved@gmail.com>
2021-12-02 20:47:40 +0100

    Rework Apaxiaaans

--- haskell/Apaxiaaans.hs
+++ haskell/Apaxiaaans.hs
@@ -1,11 +1,16 @@
 module Main where
 
-import Data.List (group)
-
--- TODO Implement group by hand
+myGroup :: String -> [String]
+myGroup [] = []
+myGroup (x:xs) = go [x] x xs
+  where
+    go acc c [] = [acc]
+    go acc c (y:ys)
+      | y == c = go (acc ++ [y]) c ys
+      | otherwise = acc : go [y] y ys
 
 run :: String -> String
-run = map head . group
+run = map head . myGroup
 
 main :: IO ()
 main = interact run
